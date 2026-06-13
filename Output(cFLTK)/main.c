966399fd9fe436b1dd0dcd1ce48e8e00287660a4
SHALAWA <bambukvova3@gmail.com>
2026-06-13 12:21:29 +0300

    Add files via upload

---
+++ Output(cFLTK)/main.c
@@ -0,0 +1,12 @@
+#include <cfltk2/cfl.h>
+#include <cfltk2/cfl_window.h>
+#include <cfltk2/cfl_output.h>
+
+int main(void) {
+    Fl_init_all();
+    Fl_Window *win = Fl_Window_new(800, 600, NULL);
+    Fl_Output *txt = Fl_Output_new(10, 20, 30, 40, "Welcome!");
+    Fl_Window_end(w);
+    Fl_Window_show(w);
+    return Fl_run();
+}
