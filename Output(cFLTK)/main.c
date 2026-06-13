d9f481414c29105ecdd96c36e89d658b0e2328ba
SHALAWA <bambukvova3@gmail.com>
2026-06-13 12:24:23 +0300

    Fix window variable name in main.c

--- Output(cFLTK)/main.c
+++ Output(cFLTK)/main.c
@@ -6,7 +6,7 @@ int main(void) {
     Fl_init_all();
     Fl_Window *win = Fl_Window_new(800, 600, NULL);
     Fl_Output *txt = Fl_Output_new(10, 20, 30, 40, "Welcome!");
-    Fl_Window_end(w);
-    Fl_Window_show(w);
+    Fl_Window_end(win);
+    Fl_Window_show(win);
     return Fl_run();
 }
