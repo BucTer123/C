dcde4b795072a5f5ab4c5ed1dcc51ca17d1e9bd1
SHALAWA <bambukvova3@gmail.com>
2026-06-14 11:51:09 +0300

    Add files via upload

---
+++ Output(GTK)/main.c
@@ -0,0 +1,12 @@
+#include <gtk/gtk.h>
+
+int main(int argc, char** argv) {
+    GtkWidget *window = gtk_new_window(GTK_TOPLEVEL);
+    gtk_window_set_label(GTK_WINDOW(window), "Output");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+
+    GtkWidget *txt = gtk_new_label("Text");
+    gtk_container_add(GTK_CONTAINER(window), txt);
+
+    return 0;
+}
