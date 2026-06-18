03dd17b06e471933aa0acf0ae26ca24994d3e24b
SHALAWA <bambukvova3@gmail.com>
2026-06-18 07:01:48 +0300

    Initialize GTK in main function

--- Output(GTK)/main.c
+++ Output(GTK)/main.c
@@ -1,6 +1,7 @@
 #include <gtk/gtk.h>
 
 int main(int argc, char** argv) {
+    gtk_init(argc, argv);
     GtkWidget *window = gtk_new_window(GTK_TOPLEVEL);
     gtk_window_set_label(GTK_WINDOW(window), "Output");
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
