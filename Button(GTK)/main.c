123d8d2a6f9052175881b11985101e9c293c4176
SHALAWA <bambukvova3@gmail.com>
2026-06-18 07:02:20 +0300

    Update main.c

--- Button(GTK)/main.c
+++ Button(GTK)/main.c
@@ -5,6 +5,8 @@ void button_callback() {
 }
 
 int main(int argc, char** argv) {
+    gtk_init(argc, argv);
+    
     GtkWidget *window, *button;
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
