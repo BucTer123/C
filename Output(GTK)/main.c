bf9c3d1e58c12b85b7e6fd3bf498244f6556d246
SHALAWA <bambukvova3@gmail.com>
2026-06-27 15:39:18 +0300

    Update main.c

--- Output(GTK)/main.c
+++ Output(GTK)/main.c
@@ -1,7 +1,7 @@
 #include <gtk/gtk.h>
 
 int main(int argc, char** argv) {
-    gtk_init(argc, argv);
+    gtk_init(&argc, &argv);
     GtkWidget *window = gtk_new_window(GTK_TOPLEVEL);
     gtk_window_set_label(GTK_WINDOW(window), "Output");
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
@@ -9,5 +9,9 @@ int main(int argc, char** argv) {
     GtkWidget *txt = gtk_new_label("Text");
     gtk_container_add(GTK_CONTAINER(window), txt);
 
+    gtk_widget_show_all(window);
+    
+    gtk_main();
+    
     return 0;
 }
