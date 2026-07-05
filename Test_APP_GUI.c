028f4811c27d7bc26bde4a2be66a95b340f1d7c2
SHALAWA <bambukvova3@gmail.com>
2026-07-05 18:22:24 +0300

    Update Test_APP_GUI.c

--- Test_APP_GUI.c
+++ Test_APP_GUI.c
@@ -1,17 +1,22 @@
 #include <gtk/gtk.h>
 
 int main(int argc, char* argv[]) {
-    GtkWidget *window, *button, label;
+    GtkWidget *window, *button, label, fixed;
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_label(GTK_WINDOW(window), "GTK");
     gtk_window_set_default_size(GTK_WINDOW(window), 640, 480);
+
+    fixed = gtk_fixed_new();
 
     label = gtk_label_new("Welcome!");
     gtk_container_add(GTK_CONTAINER(window), label);
 
     button = gtk_button_new_with_label("Click");
     gtk_container_add(GTK_CONTAINER(window), button);
+
+    gtk_fixed_put(GTK_FIXED(fixed), label, 320, 10);
+    gtk_fixed_put(GTK_FIXED(fixed), button, 320, 20);
 
     g_signal_connect(window. "clicked", G_CALLBACK(gtk_main_quit), NULL);
 
