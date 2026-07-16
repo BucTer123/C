01944c1c18945177fd8178be248b002b2f74bf16
SHALAWA <bambukvova3@gmail.com>
2026-07-16 11:18:14 +0300

    Update note_gui.c

--- Note-C/Note/note_gui.c
+++ Note-C/Note/note_gui.c
@@ -8,12 +8,16 @@ void saved_nana() {
 
     FILE *file = fopen(inp2, "w");
 
-    wind4 = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    GtkWidget *wind4 = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_default_size(GTK_WINDOW(wind4), 640, 400);
     gtk_window_set_label(GTK_WINDOW(wind4), "WINDOW!");
 
     GtkWidget *text_from_file = gtk_new_label(&file);
     g_container_add(GTK_CONTAINER(wind4), text_from_file);
+
+    GtkWidget *fixed3 = gtk_new_fixed();
+
+    gtk_fixed_put(GTK_FIXED(fixed3), 320, 10);
     
     gtk_widget_show_all(wind4);
 }
@@ -24,6 +28,8 @@ void save_note() {
     GtkWidget *wind3 = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_default_size(GTK_WINDOW(wind3), 640, 400);
     gtk_window_set_title(GTK_WINDOW(wind3), "WINDOW!");
+
+    GtkWidget *fixed2 = gtk_new_fixed();
     
     GtkWidget *inp2 = gtk_entry_new();
     gtk_entry_set_placeholder_text(GTK_ENTRY(inp2), "Name :");
@@ -31,6 +37,9 @@ void save_note() {
 
     GtkWidget *butt3 = gtk_button_new_with_label("Go");
     gtk_container_add(GTK_CONTAINER(wind3), butt3);
+
+    gtk_fixed_put(GTK_FIXED(inp2), 320, 10);
+    gtk_fixed_put(GTK_FIXED(butt3), 320, 10);
 
     g_signal_connect(butt3, "clicked", G_CALLBACK(saved_nana));
 
@@ -44,12 +53,17 @@ void nt() {
     gtk_window_set_default_size(GTK_WINDOW(wind2), 640, 400);
     gtk_window_set_title(GTK_WINDOW(wind2), "Note");
 
+    GtkWidget *fixed1 = gtk_new_fixed();
+    
     inp = gtk_entry_new();
     gtk_entry_set_placeholder_text(GTK_ENTRY(inp), "> ");
 
     butt2 = gtk_button_new_with_label("Save");
     gtk_container_add(GTK_CONTAINER(wind2), butt2);
 
+    gtk_fixed_put(GTK_FIXED(inp), 320, 10);
+    gtk_fixed_put(GTK_FIXED(butt2), 320, 20);
+    
     g_signal_connect(butt2, "clicked", G_CALLBACK(save_note));
 
     gtk_widget_show_all(wind2);
