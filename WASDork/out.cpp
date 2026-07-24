cc7c09e685955001f7360727ebe64c66db82fb22
SHALAWA <bambukvova3@gmail.com>
2026-07-24 07:54:41 +0000

    Add files via upload

---
+++ WASDork/out.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+#include "out.h"
+void wasd_dsaw(std::string wasd_text){ std::cout << wasd_text << std::endl; }
+void wasd_wasd(std::string wasd_placeholder, std::string wasd_variable){ std::string variable = wasd_variable; std::cout << wasd_placeholder << std::endl; std::cin >> variable; }
+void dsaw_dsaw(int num) { int number = num; }
+void dddd_dddd(int status) { exit(status); }
+void aaaa_aaaa(bool boolean) { bool boolean2 = boolean; }
+void ssss_ssss(float num_float) { float number_fl = num_float; }
+void waas_waad(int argc, char** argv);
---
+++ WASDork/out.h
@@ -0,0 +1,12 @@
+#ifndef OUT_H
+#define OUT_H
+
+void wasd_wasd();
+void aaaa_aaaa();
+void dddd_dddd();
+void wwww_wwww();
+void wasd_dsaw();
+void dsaw_dsaw();
+void waas_waad();
+
+#endif
