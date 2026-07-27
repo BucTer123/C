adf736867f204ec2f3db1e8fd896cd8199f2fecd
SHALAWA <bambukvova3@gmail.com>
2026-07-27 16:47:19 +0000

    Update rq.c

--- CWORK/rq.c
+++ CWORK/rq.c
@@ -3,30 +3,31 @@
 #include <stdlib.h>
 #include "rq.h"
 
-size_t Writecall (void* contents, size_t size, size_t nmemb, char [100]s) {
-	size_t totalsize = size *nmemb;
-	s->append(char*)contents, totalsize);
-	return totalsize;
-}
-
-void rq_fnc(char dom[100], int tm) {
-	for (i = 0; i < tm; i++) {
-		if (!curl) {
-			printf("ERROR!:");
-		} else {
-			CURL* curl = curl_easy_init();
-			
-			curl_easy_setopt(curl, CURLOPT_WRITEDATA, "write");
-			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, Writecall);
-			curl_easy_setopt(curl, CURLOPT_URL, dom);
-			
-			curl_easy_setopt(curl, CURLOPT_PORT, 1L);
-			curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L);
-			curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 5L); 
-			
-			curl_easy_perform(curl);
-			printf(curl);
-			curl_easy_cleanup(curl);
+struct REQ {
+	size_t Writecall (void* contents, size_t size, size_t nmemb, char [100]s) {
+		size_t totalsize = size *nmemb;
+		s->append(char*)contents, totalsize);
+		return totalsize;
+	}
+	void rq_fnc(char dom[100], int tm) {
+		for (i = 0; i < tm; i++) {
+			if (!curl) {
+				printf("ERROR!:");
+			} else {
+				CURL* curl = curl_easy_init();
+				
+				curl_easy_setopt(curl, CURLOPT_WRITEDATA, "write");
+				curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, Writecall);
+				curl_easy_setopt(curl, CURLOPT_URL, dom);
+				
+				curl_easy_setopt(curl, CURLOPT_PORT, 1L);
+				curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L);
+				curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 5L); 
+				
+				curl_easy_perform(curl);
+				printf(curl);
+				curl_easy_cleanup(curl);
+			}
 		}
 	}
-}
+};
