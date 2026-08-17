3c3e646d9f94a633430fb3dc1783a7388d4916c6
Boba <bambukvova3@gmail.com>
2026-08-17 07:45:07 +0100

    Update out.cpp

--- WASDork/out.cpp
+++ WASDork/out.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
-#include<string>
+#include <stdio.h>
+#include <boolean.h>
+#include <float.h>
 #include "out.h"
-void wasd_dsaw(std::string wasd_text){ std::cout << wasd_text << std::endl; }
-void wasd_wasd(std::string wasd_placeholder, std::string wasd_variable){ std::string variable = wasd_variable; std::cout << wasd_placeholder << std::endl; std::cin >> variable; }
+void wasd_dsaw(char wasd_text[100]){ std::cout << wasd_text << std::endl; }
+void wasd_wasd(char wasd_placeholder[100], char wasd_variable[100]){ char wasd[100] = wasd_variable; printf(wasd_placeholder.c_str()); scanf("%s", wasd); }
 void dsaw_dsaw(int num) { int number = num; }
 void dddd_dddd(int status) { exit(status); }
 void aaaa_aaaa(bool boolean) { bool boolean2 = boolean; }
