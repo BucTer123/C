4a136d6c5a36840e77d5ad2d0048ec588ba5602a
Boba <bambukvova3@gmail.com>
2026-08-26 08:33:05 +0200

    Refactor OS struct method names for clarity

--- CWORK/os.c
+++ CWORK/os.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<bool.h>
 #include<sys/stat.h>
 #include"os.h"
 
 struct OS {
-    void MKDIR(char name_dir_create[100]) { mkdir(name_dir_create); }
-    void RMDIR(char name_dir_remove[100]) { rmdir(name_dir_remove); }
-    void MKFIL(char name_file_create[100]) { FILE *file = fopen(name_file_create, "w"); }
-    void RMFIL(char name_file_remove[100]) { remove(name_file_remove) }
-    void EXEC(char name_command[100]) { system(name_command); }
+    void CREATE_DIR(char name_dir_create[100]) { mkdir(name_dir_create); }
+    void REMOVE_DIR(char name_dir_remove[100]) { rmdir(name_dir_remove); }
+    void CREATE_FILE(char name_file_create[100]) { FILE *file = fopen(name_file_create, "w"); }
+    void REMOVE_FILE(char name_file_remove[100]) { remove(name_file_remove) }
+    void EXECUTE(char name_command[100]) { system(name_command); }
+    void RETURN(bool return_status) { return return_status; }
 };
