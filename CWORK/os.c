0d3d9c8238415e0674fc5aa9fb785ba840bed843
Boba <bambukvova3@gmail.com>
2026-09-19 12:30:05 +0200

    Update os.c

--- CWORK/os.c
+++ CWORK/os.c
@@ -7,7 +7,7 @@
 struct cwork_os {
     void create_dir(char name_dir_create[100]) { mkdir(name_dir_create); }
     void remove_dir(char name_dir_remove[100]) { rmdir(name_dir_remove); }
-    void CREATE_FILE(char name_file_create[100]) { FILE *file = fopen(name_file_create, "w"); }
+    void create_file(char name_file_create[100]) { FILE *file = fopen(name_file_create, "w"); }
     void REMOVE_FILE(char name_file_remove[100]) { remove(name_file_remove) }
     void EXECUTE(char name_command[100]) { system(name_command); }
     void RETURN(bool return_status) { return return_status; }
